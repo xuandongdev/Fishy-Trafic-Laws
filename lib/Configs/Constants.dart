2a365ddcd91b87732ad3e7d5cb94e04e91639169
Bui Xuan Dong <154953010+xuandongdev@users.noreply.github.com>
2025-04-21 21:24:24 +0700

    Update Constants.dart

--- lib/Configs/Constants.dart
+++ lib/Configs/Constants.dart
@@ -1,13 +1,12 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class Constants {
-  static final String apiKey = "AIzaSyCQDCoKqIF_e36LGsg64qMkdD0XdNOTn_Q";
-  static final String baseUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
+  static final String apiKey = hehe;
+  static final String baseUrl = hehe;
 }
 class SupabaseConfig {
-  static final String supaUrl = "https://bqrvxbqthhsrswutktvr.supabase.co";
-  static final String supaKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxcnZ4YnF0aGhzcnN3dXRrdHZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzNTI1NjAsImV4cCI6MjA1ODkyODU2MH0.G0mLzBZzXKc7fI93Vl5bRLYOCyvLo3pXSop1NH0W0WI";
-
+  static final String supaUrl = hehe;
+  static final String supaKey = hehe;
   static bool _isInitialized = false;
 
   static Future<void> initialize() async {
